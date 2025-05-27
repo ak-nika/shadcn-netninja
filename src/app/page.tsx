43693f37f5ffd0193eaad8dce1441b0c34a420e5
ak-nika <akingbayiojo@gmail.com>
2025-05-27 13:28:07 +0100

    card components complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,68 @@
-const Home = () => {
-  return <main>Home</main>;
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+
+interface Recipes {
+  id: string;
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+}
+
+const Home = async () => {
+  const getRecipes = async (): Promise<Recipes[]> => {
+    const res = await fetch("http://localhost:5000/recipes");
+
+    return res.json();
+  };
+
+  const recipes = await getRecipes();
+
+  return (
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex flex-row gap-4 items-center">
+              <Avatar>
+                <AvatarImage
+                  src={`/images/${recipe.image}`}
+                  alt={recipe.title}
+                />
+                <AvatarFallback>
+                  {recipe.title.charAt(0).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} minutes to cook</CardDescription>
+              </div>
+            </CardHeader>
+
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+
+            <CardFooter className="flex justify-between items-center">
+              <Button className="cursor-pointer">View Recipe</Button>
+              {recipe.vegan && <Badge variant="secondary">Vegan</Badge>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default Home;
